9fc0e64799ace868920994d1e409d88798789be2
MarcosSantos95 <marcos.santos.dev@gmail.com>
2024-02-06 16:48:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let formulario = document.querySelector(".fale-conosco")
+let mascara = document.querySelector(".mascara-form")
+
+
+
+function cliqueiNoBotao () {
+    
+    formulario.style.left = "700px"
+    mascara.style.visibility = "visible"
+}
+
+function sumirFormulario() {
+    formulario.style.left = "-700px"
+    mascara.style.visibility = "hidden"
+
+}
